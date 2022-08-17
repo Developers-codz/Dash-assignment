b5b2c86e158f5a754874a171da79d63409d914a4
developers-codz <kaj123al2002@gmail.com>
2022-08-17 23:46:12 +0530

    fix:drawer in auth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,useLocation } from "react-router-dom";
 import { Dashboard, Login, Setting, Signup, Users } from "./pages";
 import { useAside } from "./context/asideContext";
 
+
 function App() {
   const { isDrawerOpen, setDrawerOpen } = useAside();
+  const {pathname} = useLocation();
   const clickHandler = () => {
     setDrawerOpen((prev) => !prev);
   };
   return (
     // drawer-close  drawer-open
     <div className="App ">
-      <div
+      {pathname !== "/" && pathname !=="signup" ? <div
         className={`fixed inset-y-1/4 -left-8 drawer z-10 bg-color-blue text-white w-16 h-16 rounded-full  ${
           isDrawerOpen ? "left-1/2" : "inset-x-0" 
         } sm:invisible`}
@@ -22,7 +24,7 @@ function App() {
           ) : (
             <i className="fa fa-angle-double-right fa-lg" aria-hidden="true"></i>
         )}
-      </div>
+      </div> : <></> }
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="signup" element={<Signup />} />
